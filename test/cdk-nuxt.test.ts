3edc1e1c6129f8ba886e4795cc4bd2722cc72835
Ferdinand Frank <info@ferdinandfrank.com>
2022-02-07 10:22:50 +0100

    Initial commit

---
+++ test/cdk-nuxt.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkNuxt from '../lib/cdk-nuxt-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-nuxt-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkNuxt.CdkNuxtStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
