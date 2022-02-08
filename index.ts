523540857f2da36a07e35d94ac9137ce60b3e3cf
Ferdinand Frank <info@ferdinandfrank.com>
2022-02-08 08:16:42 +0100

    Move index file to root

---
+++ index.ts
@@ -0,0 +1,2 @@
+export {NuxtAppStack, NuxtAppStackProps} from "./lib/stack/nuxt-app-stack"
+export {NuxtAppAssetsCleanupStack, NuxtAppAssetsCleanupProps} from "./lib/stack/nuxt-app-assets-cleanup-stack"
