18680e3f0e3e33fb008c54614d592bfdf1397465
Ferdinand Frank <info@ferdinandfrank.com>
2022-02-09 15:20:13 +0100

    Publish AppStackProps

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
+export {AppStackProps} from "./lib/stack/app-stack-props"
 export {NuxtAppStack, NuxtAppStackProps} from "./lib/stack/nuxt-app-stack"
 export {NuxtAppAssetsCleanupStack, NuxtAppAssetsCleanupProps} from "./lib/stack/nuxt-app-assets-cleanup-stack"
